815717c2b95bdede243597c3c41ba9d4fcbce576
root <root@localhost.localdomain>
2016-10-07 02:58:26 +0100

    first commit

---
+++ first/hello.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "INDEX PAGE"
+
+@app.route('/hello')
+def hello():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
+
